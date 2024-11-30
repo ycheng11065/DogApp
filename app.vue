27edc620cbfab8188beffba6b0fa4bb01254c290
ycheng11065 <ychengli11065@gmail.com>
2024-11-30 15:33:46 -0800

    can see dog pictures now

--- app.vue
+++ app.vue
@@ -1,19 +1,22 @@
+<script setup>
 
-
-<script setup lang="ts">
-
-const { data, status, error, refresh, clear } = await useAsyncData(
-  'dogs',
-  () => $fetch('https://dog.ceo/api/breeds/image/random')
+const { data, status, error, refresh, clear } = await useAsyncData('dogs', () => 
+  $fetch('https://dog.ceo/api/breeds/image/random')
 );
 
 console.log(data.value);
 </script>
 
 <template>
-  <div>
+  <div class="app">
+    <div class="buttons">
+      <button class="dogButton" @click=refresh>Create Dog</button>
+    </div>
+    <div class="dogImage">
+      <img :src=data.message />
+    </div>
     <p> {{data.message}} </p>
-    <img :src=data.message />
+    
   </div>
 </template>
 
