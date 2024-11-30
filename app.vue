defbd1ad022cc1946aaa73b342acf4323e9f9303
ycheng11065 <ychengli11065@gmail.com>
2024-11-30 15:20:12 -0800

    connection to api

--- app.vue
+++ app.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
+    <p>Hello world!</p>
 
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+
+const { data, status, error, refresh, clear } = await useAsyncData(
+  'dogs',
+  () => $fetch('https://dog.ceo/api/breeds/image/random')
+);
+
+console.log(data.value);
 </script>
 
 <style lang="scss" scoped>
